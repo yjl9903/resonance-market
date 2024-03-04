e4a00c24930242cae64825489542acacaedd5955
XLor <yjl9903@vip.qq.com>
2024-03-05 01:07:01 +0800

    chore: some things

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-
+  <div class="main">
+    <h1 class="pt-8 pb-4 text-2xl font-bold">雷索纳斯市场</h1>
+  </div>
 </template>
+
+<style>
+.main {
+  @apply: mx-auto xl:w-5xl lg:w-3xl md:w-2xl lt-md:w-[95vw] lt-md:px-3 lt-sm:w-[100vw];
+}
+</style>
