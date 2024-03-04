d34647c23509e1ad7e1762cf06e6a4508b1dd8a0
XLor <yjl9903@vip.qq.com>
2024-03-05 05:07:02 +0800

    feat: add hello toast

--- app.vue
+++ app.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { toast } from 'vue-sonner';
+
 import { Toaster } from '@/components/ui/sonner';
+
+onMounted(() => {
+  toast(`网站仍在开发中`, {
+    description: '建议反馈: https://github.com/yjl9903/resonance-market/issues'
+  });
+});
 </script>
 
 <template>
-  <Toaster richColors position="top-center" />
+  <Toaster richColors closeButton position="top-right" />
   <NuxtLoadingIndicator />
   <NuxtLayout>
     <NuxtLoadingIndicator />
