eca83a2a5d92a5f6d1804bf3b04e452489caf34a
XLor <yjl9903@vip.qq.com>
2024-03-06 12:10:59 +0800

    fix: change log invalid time

--- utils/log.ts
+++ utils/log.ts
@@ -3,5 +3,6 @@ import type { Log } from '~/drizzle/schema';
 export function isLogValid(log: Log | undefined | null): log is Log {
   if (!log) return false;
   const uploadedAt = log.uploadedAt.getTime();
-  return new Date().getTime() - uploadedAt > 3600 * 1000;
+  // 30 分钟
+  return new Date().getTime() - uploadedAt > 1800 * 1000;
 }
