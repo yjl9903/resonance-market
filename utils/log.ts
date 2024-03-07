9c888cdb7259c2b91234357a9b047a04724cbfd0
luqi09 <luqi09@meituan.com>
2024-03-07 18:42:25 +0800

    fix: update invalid detection time

--- utils/log.ts
+++ utils/log.ts
@@ -4,5 +4,5 @@ export function isLogValid(log: Log | undefined | null): log is Log {
   if (!log) return false;
   const uploadedAt = log.uploadedAt.getTime();
   // 30 分钟
-  return new Date().getTime() - uploadedAt > 1800 * 1000;
+  return new Date().getTime() - uploadedAt > 3600 * 1000;
 }
