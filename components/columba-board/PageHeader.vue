1ffed397a194c0b4517f9c98d03a094d9c08d014
Ximu-Luya <794876939@qq.com>
2024-03-07 20:26:01 +0800

    feat: 新增子页面框架

---
+++ components/columba-board/PageHeader.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string
+  backgroundColorClass?: string
+}>()
+const router = useRouter()
+</script>
+
+<template>
+  <header class="px-3" :class="[backgroundColorClass, {'text-white': backgroundColorClass}]">
+    <div class="flex items-center h-12">
+      <div
+        class="mx-2 rounded-full hover:bg-gray-200 cursor-pointer flex items-center justify-center w-8 h-8" 
+        @click="router.back()"
+      >
+        <span class="i-material-symbols:keyboard-backspace-rounded w-6 h-6"></span>
+      </div>
+      <h1 class="text-xl">{{ props.title }}</h1>
+    </div>
+  </header>
+</template>
---
+++ layouts/child-page.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string
+  headerBackgroundColorClass?: string
+}>()
+</script>
+
+<template>
+  <div class="h-screen flex-col flex">
+    <PageHeader :title="props.title" :background-color-class="headerBackgroundColorClass" />
+    <section class="page-wrapper flex-grow flex-shrink overflow-y-auto p-3">
+      <slot></slot>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  font-weight: normal;
+}
+
+body {
+  min-height: 100vh;
+  color: var(--color-text);
+  background: var(--color-background);
+  transition:
+    color 0.5s,
+    background-color 0.5s;
+  line-height: 1.6;
+  font-family:
+    Inter,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    Oxygen,
+    Ubuntu,
+    Cantarell,
+    'Fira Sans',
+    'Droid Sans',
+    'Helvetica Neue',
+    sans-serif;
+  font-size: 15px;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.page-wrapper {
+  background-color: #F6F6F6;
+}
+</style>
