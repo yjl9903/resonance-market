213e1377defab8f34c29e1050476bab125f45edb
luqi09 <luqi09@meituan.com>
2024-03-07 18:50:28 +0800

    chore: update comment

--- utils/log.ts
+++ utils/log.ts
@@ -3,6 +3,6 @@ import type { Log } from '~/drizzle/schema';
 export function isLogValid(log: Log | undefined | null): log is Log {
   if (!log) return false;
   const uploadedAt = log.uploadedAt.getTime();
-  // 30 分钟
+  // 60 分钟
   return new Date().getTime() - uploadedAt > 3600 * 1000;
 }
