5c97b07dfd0aa0a6c7e23ecaf2cf50fdec60ff1c
Ximu-Luya <794876939@qq.com>
2024-03-08 02:12:24 +0800

    style: 调整货物详情页中货物信息的展示

--- components/columba-board/page/ProductDetail/ProductInfo.vue
+++ components/columba-board/page/ProductDetail/ProductInfo.vue
@@ -8,11 +8,11 @@ const { productInfo } = defineProps<{
 </script>
 
 <template>
-  <div class="flex flex-col bg-blue-400 text-white px-3 pt-3 -mx-3 -mt-3 pb-20 -mb-18">
-    <div class="text-sm">
+  <div class="flex flex-col bg-blue-400 text-white px-3 pt-3 -mx-3 -mt-3 pb-20 -mb-16">
+    <div class="text-xs">
       原产地：{{ productInfo.originCities.map(city => city.name).join(', ') }}
     </div>
-    <div class="text-2xl font-bold">{{ productInfo.name }}</div>
+    <div class="text-2xl font-bold my-1">{{ productInfo.name }}</div>
     <div class="flex flex-row space-x-2 overflow-x-auto mt-1">
       <span class="px-2 py-1 text-xs bg-yellow-500 rounded-md">特产</span>
     </div>
