3a0c2f3eaf94c2f2eba51813ad6493b7bdff1ca4
XLor <yjl9903@vip.qq.com>
2024-03-15 00:03:51 +0800

    feat: query products and cities info

---
+++ server/api/info/products.get.ts
@@ -0,0 +1,6 @@
+import { products } from '~/utils/cities';
+
+export default defineEventHandler(async (event) => {
+  setHeader(event, 'cache-control', `max-age=${3600 * 24}`);
+  return products;
+});
