77a69b804af1896a0383a65ef7230d767203aa3d
XLor <yjl9903@vip.qq.com>
2024-03-14 23:39:28 +0800

    fix: trim prefix

--- server/api/resodata/[...slug].ts
+++ server/api/resodata/[...slug].ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   const url = getRequestURL(event);
   const req = toWebRequest(event);
-  const pathname = url.pathname.slice('/resodata'.length);
-  return fetch(`https://reso-data.kmou424.moe/${pathname}`, req);
+  const pathname = url.pathname.slice('/api/resodata'.length);
+  return fetch(`https://reso-data.kmou424.moe${pathname}`, req);
 });
