2a1acfee979c9534916960d133146806709442fd
XLor <yjl9903@vip.qq.com>
2024-03-15 00:00:08 +0800

    fix: pass search params

--- server/api/resodata/[...slug].ts
+++ server/api/resodata/[...slug].ts
@@ -2,5 +2,5 @@ export default defineEventHandler(async (event) => {
   const url = getRequestURL(event);
   const req = toWebRequest(event);
   const pathname = url.pathname.slice('/api/resodata'.length);
-  return fetch(`https://reso-data.kmou424.moe${pathname}`, req);
+  return fetch(`https://reso-data.kmou424.moe${pathname}${url.search}`, req);
 });
