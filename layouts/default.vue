997a76d1e2a45e75f10e628646b0ab955aa2d9df
Ximu-Luya <794876939@qq.com>
2024-03-17 22:43:51 +0800

    chore: 代码风格调整

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,9 +26,7 @@ const settingStore = useSettingStore()
     <nav class="border-b pt-12 mb-4">
       <div class="main">
         <div>
-          <NuxtLink to="/" class="text-2xl font-bold hover:text-base-600 select-none"
-            >雷索纳斯市场</NuxtLink
-          >
+          <NuxtLink to="/" class="text-2xl font-bold hover:text-base-600 select-none">雷索纳斯市场</NuxtLink>
         </div>
         <Menubar class="mt-4">
           <MenubarButton>
@@ -41,9 +39,7 @@ const settingStore = useSettingStore()
                 <MenubarSubTrigger>{{ city.name }}</MenubarSubTrigger>
                 <MenubarSubContent>
                   <MenubarItem v-for="product in city.products" :key="product.name" as-child>
-                    <NuxtLink :to="`/product/${city.name}/${product.name}`">{{
-                      product.name
-                    }}</NuxtLink>
+                    <NuxtLink :to="`/product/${city.name}/${product.name}`">{{ product.name }}</NuxtLink>
                   </MenubarItem>
                 </MenubarSubContent>
               </MenubarSub>
@@ -55,21 +51,17 @@ const settingStore = useSettingStore()
               <MenubarSub>
                 <MenubarSubTrigger>在城市买入</MenubarSubTrigger>
                 <MenubarSubContent>
-                  <MenubarItem v-for="city in cities" :key="city.name" as-child
-                    ><NuxtLink :to="`/report/buy/${city.name}`">{{
-                      city.name
-                    }}</NuxtLink></MenubarItem
-                  >
+                  <MenubarItem v-for="city in cities" :key="city.name" as-child>
+                    <NuxtLink :to="`/report/buy/${city.name}`">{{ city.name }}</NuxtLink>
+                  </MenubarItem>
                 </MenubarSubContent>
               </MenubarSub>
               <MenubarSub>
                 <MenubarSubTrigger>在城市卖出</MenubarSubTrigger>
                 <MenubarSubContent>
-                  <MenubarItem v-for="city in cities" :key="city.name" as-child
-                    ><NuxtLink :to="`/report/sell/${city.name}`">{{
-                      city.name
-                    }}</NuxtLink></MenubarItem
-                  >
+                  <MenubarItem v-for="city in cities" :key="city.name" as-child>
+                    <NuxtLink :to="`/report/sell/${city.name}`">{{ city.name }}</NuxtLink>
+                  </MenubarItem>
                 </MenubarSubContent>
               </MenubarSub>
             </MenubarContent>
@@ -107,70 +99,18 @@ const settingStore = useSettingStore()
             <MenubarTrigger>关于</MenubarTrigger>
             <MenubarContent>
               <MenubarItem as-child>
-                <a href="https://github.com/yjl9903/resonance-market" target="_blank"
-                  ><span i-carbon-logo-github mr-1 block w-4></span><span>GitHub</span></a
-                >
+                <a href="https://github.com/yjl9903/resonance-market" target="_blank">
+                  <span i-carbon-logo-github mr-1 block w-4></span><span>GitHub</span>
+                </a>
               </MenubarItem>
               <MenubarItem as-child>
-                <a href="https://space.bilibili.com/1631015691" target="_blank"
-                  ><span i-simple-icons-bilibili mr-1 block w-4></span
-                  ><span>雷索纳斯官方 Bilibili</span></a
-                >
+                <a href="https://space.bilibili.com/1631015691" target="_blank">
+                  <span i-simple-icons-bilibili mr-1 block w-4></span>
+                  <span>雷索纳斯官方 Bilibili</span>
+                </a>
               </MenubarItem>
             </MenubarContent>
           </MenubarMenu>
-          <!-- <MenubarMenu>
-            <MenubarTrigger>Edit</MenubarTrigger>
-            <MenubarContent>
-              <MenubarItem> Undo <MenubarShortcut>⌘Z</MenubarShortcut> </MenubarItem>
-              <MenubarItem> Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut> </MenubarItem>
-              <MenubarSeparator />
-              <MenubarSub>
-                <MenubarSubTrigger>Find</MenubarSubTrigger>
-                <MenubarSubContent>
-                  <MenubarItem>Search the web</MenubarItem>
-                  <MenubarSeparator />
-                  <MenubarItem>Find...</MenubarItem>
-                  <MenubarItem>Find Next</MenubarItem>
-                  <MenubarItem>Find Previous</MenubarItem>
-                </MenubarSubContent>
-              </MenubarSub>
-              <MenubarSeparator />
-              <MenubarItem>Cut</MenubarItem>
-              <MenubarItem>Copy</MenubarItem>
-              <MenubarItem>Paste</MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-          <MenubarMenu>
-            <MenubarTrigger>View</MenubarTrigger>
-            <MenubarContent>
-              <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
-              <MenubarCheckboxItem checked> Always Show Full URLs </MenubarCheckboxItem>
-              <MenubarSeparator />
-              <MenubarItem inset> Reload <MenubarShortcut>⌘R</MenubarShortcut> </MenubarItem>
-              <MenubarItem disabled inset>
-                Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
-              </MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem inset> Toggle Fullscreen </MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem inset> Hide Sidebar </MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-          <MenubarMenu>
-            <MenubarTrigger>Profiles</MenubarTrigger>
-            <MenubarContent>
-              <MenubarRadioGroup value="benoit">
-                <MenubarRadioItem value="andy"> Andy </MenubarRadioItem>
-                <MenubarRadioItem value="benoit"> Benoit </MenubarRadioItem>
-                <MenubarRadioItem value="Luis"> Luis </MenubarRadioItem>
-              </MenubarRadioGroup>
-              <MenubarSeparator />
-              <MenubarItem inset> Edit... </MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem inset> Add Profile... </MenubarItem>
-            </MenubarContent>
-          </MenubarMenu> -->
         </Menubar>
       </div>
     </nav>
