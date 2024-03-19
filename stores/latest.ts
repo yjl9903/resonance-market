73e8f1df82379dcae86ae840703d503193a3052f
Ximu-Luya <794876939@qq.com>
2024-03-20 02:51:00 +0800

    feat: 将数据获取循环逻辑迁移到store中

--- stores/latest.ts
+++ stores/latest.ts
@@ -4,7 +4,7 @@ export const useLatestLogs = defineStore('latest_logs', () => {
   const logs = ref<Log[]>([])
   const transactionMap = ref<Map<string, Log>>(new Map())
 
-  const fetch = async () => {
+  const startGetData = async () => {
     const resp = await $fetch(`/api/product`, { retry: 3 })
     logs.value = resp.latest.map(log => ({ ...log, uploadedAt: new Date(log.uploadedAt) }))
     // 将数据转换为 Map 方便查询
@@ -13,6 +13,9 @@ export const useLatestLogs = defineStore('latest_logs', () => {
       const key = `${log.name}-from${log.sourceCity}to${log.targetCity}`
       transactionMap.value.set(key, log)
     })
+
+    // 每隔 10 秒重新获取数据
+    setTimeout(startGetData, 10 * 1000)
   }
 
   return {
@@ -20,6 +23,6 @@ export const useLatestLogs = defineStore('latest_logs', () => {
     getLatestLog(sourceCity: string, productName: string, targetCity: string) {
       return transactionMap.value.get(`${productName}-from${sourceCity}to${targetCity}`)
     },
-    fetch
+    startGetData,
   }
 })
