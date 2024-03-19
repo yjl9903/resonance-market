cf4587ff84858ebe125c780b58f724cb3f002271
Ximu-Luya <794876939@qq.com>
2024-03-20 00:38:36 +0800

    feat: 首页数据表格支持城市筛选

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,32 @@ await store.fetch();
 useIntervalFn(async () => {
   await store.fetch();
 }, 10 * 1000);
+
+const selectedCity = ref<CityInfo[]>([cities[0]])
+
+const switchCityFilter = (targetCity: CityInfo) => {
+  if (selectedCity.value.find(city => city.name == targetCity.name)) {
+    selectedCity.value = selectedCity.value.filter(city => city.name !== targetCity.name);
+  } else {
+    selectedCity.value = [...selectedCity.value, targetCity];
+  }
+};
 </script>
 
 <template>
   <div class="main pb-12">
+    <div class="flex space-x-4 mb-4">
+      <Button
+        v-for="city in cities"
+        :key="city.name"
+        @click="switchCityFilter(city)"
+        :variant="selectedCity.find(item => item.name == city.name) ? 'default' : 'outline'"
+        size="sm"
+      >{{ city.name }}</Button>
+    </div>
+
     <div class="space-y-4">
-      <City v-for="city in cities" :key="city.name" :city="city"></City>
+      <City v-for="city in selectedCity" :key="city.name" :city="city"></City>
     </div>
   </div>
 </template>
