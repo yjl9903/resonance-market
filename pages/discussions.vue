c681544b5dfafc708a34172407c8f57f16bbcaf3
Ximu-Luya <794876939@qq.com>
2024-03-20 00:17:19 +0800

    feat: 将讨论区迁移到独立的页面

---
+++ pages/discussions.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Giscus from '@giscus/vue'
+</script>
+
+<template>
+  <div class="main pb-12">
+    <ClientOnly>
+      <div class="pt-8">
+        <Giscus
+          id="comments"
+          repo="yjl9903/resonance-market"
+          repoId="R_kgDOLbiZ1A"
+          mapping="number"
+          term="7"
+          reactionsEnabled="0"
+          emitMetadata="0"
+          inputPosition="top"
+          theme="preferred_color_scheme"
+          lang="zh-CN"
+        />
+      </div>
+    </ClientOnly>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import Giscus from '@giscus/vue';
-
 const store = useLatestLogs();
 await store.fetch();
 
@@ -14,21 +12,5 @@ useIntervalFn(async () => {
     <div class="space-y-4">
       <City v-for="city in cities" :key="city.name" :city="city"></City>
     </div>
-    <ClientOnly>
-      <div class="pt-8">
-        <Giscus
-          id="comments"
-          repo="yjl9903/resonance-market"
-          repoId="R_kgDOLbiZ1A"
-          mapping="number"
-          term="7"
-          reactionsEnabled="0"
-          emitMetadata="0"
-          inputPosition="bottom"
-          theme="preferred_color_scheme"
-          lang="zh-CN"
-        />
-      </div>
-    </ClientOnly>
   </div>
 </template>
