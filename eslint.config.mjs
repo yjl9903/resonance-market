1dc70c8e4cd0ead2c37fe9e81670c5a78a168f94
Ximu-Luya <794876939@qq.com>
2024-03-23 16:48:16 +0800

    feat: 针对项目当前代码风格覆写部分eslint规则

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,35 @@
 import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
-  // your custom flat configs go here, for example:
-  // {
-  //   files: ['**/*.ts', '**/*.tsx'],
-  //   rules: {
-  //     'no-console': 'off' // allow console.log in TypeScript files
-  //   }
-  // },
-  // {
-  //   ...
-  // }
+  {
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'off', // @ts-ignore注释的使用限制
+      '@stylistic/arrow-parens': 'off', // 箭头函数参数括号规则
+      '@typescript-eslint/no-explicit-any': 'off' // any类型限制
+    }
+  },
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/attribute-hyphenation': 'off', // 模板属性是否必须使用连字符
+      'vue/max-attributes-per-line': ['warn', { // 单行最大属性数
+        singleline: {
+          max: 3 // 单行最大属性数
+        },
+        multiline: {
+          max: 1 // 多行最大属性数
+        }
+      }],
+      'vue/attributes-order': 'off', // 属性顺序
+      'vue/multi-word-component-names': 'off', // 组件名多个单词
+      'vue/html-self-closing': ['error', { // 自闭合标签
+        html: {
+          void: 'always', // 空标签，强制自闭合
+          normal: 'any', // 普通HTML标签，不强制自闭合
+          component: 'always' // 组件，强制自闭合
+        }
+      }],
+      'vue/singleline-html-element-content-newline': 'off' // 单行元素内容换行
+    }
+  }
 );
